3d4ed84de51fd7e6e60bce5f2b6bdbae0a5f0106
Ardi Febrian <ardifebri135@gmail.com>
2024-03-04 15:06:13 +0700

    change

--- 04_Branch-Loop-Function-Dart/Praktikum/soal_prioritas_1/1_tugas_percabangan.dart
+++ 04_Branch-Loop-Function-Dart/Praktikum/soal_prioritas_1/1_tugas_percabangan.dart
@@ -1,16 +1,25 @@
+// Fungsi getGrade menerima parameter nilai dan mengembalikan string berdasarkan kriteria nilai.
 String getGrade(int nilai) {
+  // Jika nilai lebih besar dari 70, maka kembali "Nilai A".
   if (nilai > 70) {
     return "Nilai A";
-  } else if (nilai > 40) {
+  }
+  // Jika nilai lebih besar dari 40 (dan kurang dari atau sama dengan 70), kembali "Nilai B".
+  else if (nilai > 40) {
     return "Nilai B";
-  } else if (nilai > 0) {
+  }
+  // Jika nilai lebih besar dari 0 (dan kurang dari atau sama dengan 40), kembali "Nilai C".
+  else if (nilai > 0) {
     return "Nilai C";
-  } else {
+  }
+  // Jika nilai tidak memenuhi kondisi di atas, kembali "kosong".
+  else {
     return "kosong"; // untuk nilai yang tidak memenuhi kondisi
   }
 }
 
 void main() {
   int nilai = 75; // Anda dapat mengubah nilai di sini
+  // Memanggil fungsi getGrade dengan nilai yang telah ditentukan dan mencetak hasilnya.
   print(getGrade(nilai));
 }
